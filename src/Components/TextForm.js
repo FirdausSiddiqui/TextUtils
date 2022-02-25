2d1e529e016eafbe31f8c84b4c0a21ebb54e0d9e
FirdausSiddiqui <firdaussiddiqui2@gmail.com>
2022-02-26 00:19:28 +0530

    add copytext and remove extra space function

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 
 export default function TextForm(){
 
-    const [text ,setText] =useState(" ");
+    const [text ,setText] =useState("");
+    const [mode ,setMode]=useState({           
+        color:"black",
+        backgroundColor:"white"
+    });
+    const [modeButton,setModeButton]=useState("Enable Dark Mode");
 
     function changeHandler(event){
         const t=event.target.value;
@@ -19,16 +24,60 @@ export default function TextForm(){
         setText(upperText);
     }
     function handleReset(){
-        setText(" ")
+        setText("")
     }
+ 
+    function handleMode(){
+        if(mode.color==="white"){
+            setMode({
+                color:"black",
+                backgroundColor:"white"
+            });
+            setModeButton("Enable Dark Mode");
+        }
+        if(mode.color==="black"){
+            setMode({
+                color:"white",
+                backgroundColor:"black"
+            });
+            setModeButton("Enable Light Mode");
+
+        }
+    }
+
+    function copyText(){
+        let data = document.getElementById("mybox");
+        data.select();
+        navigator.clipboard.writeText(data.value);
+
+    }
+    function removeSpaces(){
+        // const newString = text.replace(/\s+/g,' ').trim();
+        // setText(newString);
+        
+        const newString = text.split(/[ ]+/);
+        setText(newString.join(" "));
+        
+        
+    }
+
+    
+
     return(      
-        <div className="conatiner">           
-            <div className="m-5 p-5 container">    
-            <h1 className="Heading">Text Utills</h1>       
-            <textarea className="form-control" value={text} onChange={changeHandler} rows="8" />
-            <button type="button" className="btn btn-primary m-2" onClick={handleClick}>Convert To Uppercase</button>
-            <button type="button" className="btn btn-primary m-2" onClick={handleLower}>Convert To Lowercase</button>
-            <button type="button" className="btn btn-primary m-2" onClick={handleReset}>Reset</button>
+        <div  className="container" style={mode}>           
+            <div className="my-3  container" style={mode}>    
+                <h1 className="Heading">Enter Text To Analyze</h1>       
+                <textarea className="form-control" id="mybox" value={text} onChange={changeHandler} rows="8" />
+                <button type="button" className="btn btn-primary m-2" onClick={handleClick}>Convert To Uppercase</button>
+                <button type="button" className="btn btn-primary m-2" onClick={handleLower}>Convert To Lowercase</button>
+                <button type="button" className="btn btn-primary m-2" onClick={handleReset}>Clear Text</button>      
+                <button type="button" className="btn btn-primary m-2" onClick={copyText}>Copy Text</button>
+                <button type="button" className="btn btn-primary m-2" onClick={removeSpaces}>Remove Extra Space</button>
+                <button type="button" className="btn btn-primary m-2" onClick={handleMode}>{modeButton}</button>
+            </div>
+            <div className="container my-3">
+                <h2>Your text summary here</h2>
+                <p>Number of Words = {(text==="")?0:text.split(" ").length} and Number of character ={text.length}</p>
             </div>
         </div>          
     )
